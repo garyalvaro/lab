8a3ef7154b5b4d7bd7a940c13a278ab52580a8b9
Gary Alvaro <garyalvarowijaya@users.noreply.github.com>
2019-11-30 16:17:55 +0700

    folder sql

---
+++ sql/aslab(3011).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2019 at 10:14 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aslab`
+--
+
+CREATE TABLE `aslab` (
+  `id_aslab` int(11) NOT NULL,
+  `nim` varchar(15) NOT NULL,
+  `nama_aslab` varchar(100) NOT NULL,
+  `tahun_ajaran` varchar(10) NOT NULL,
+  `status` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aslab`
+--
+
+INSERT INTO `aslab` (`id_aslab`, `nim`, `nama_aslab`, `tahun_ajaran`, `status`) VALUES
+(1, '181402031', 'Gary Alvaro', '2019/2020', '0'),
+(2, '181402064', 'Xixillia Sunaryo', '2019/2020', '1'),
+(3, '171402030', 'Taufik Rorkyendo', '2019/2020', '1'),
+(4, '161402100', 'Sinta', '2019/2020', '1'),
+(5, '171402117', 'Annisa Assya Mawaddah', '2019/2020', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aslab`
+--
+ALTER TABLE `aslab`
+  ADD PRIMARY KEY (`id_aslab`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aslab`
+--
+ALTER TABLE `aslab`
+  MODIFY `id_aslab` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
